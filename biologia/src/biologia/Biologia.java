9ce70ebff75b998ae9a012c6bbf44a7e68921df3
Royel2000 <113474109+Royel2000@users.noreply.github.com>
2022-12-01 17:38:06 -0500

    dic12

---
+++ biologia/src/biologia/Biologia.java
@@ -0,0 +1,11 @@
+package biologia;
+
+public class Biologia {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana v=new Ventana();
+        v.setVisible(true);
+    }
+    
+}
